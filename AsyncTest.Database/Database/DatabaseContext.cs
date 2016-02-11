8c109da8b299ee6823290a0122f3127e455cacc6
BinaryTENSHi <binarytenshi@gmail.com>
2016-02-11 11:42:03 +0100

    Common project base

--- AsyncTest.Database/Database/DatabaseContext.cs
+++ AsyncTest.Database/Database/DatabaseContext.cs
@@ -1,8 +1,10 @@
 ﻿using System.Data.Entity;
+using System.Diagnostics.CodeAnalysis;
 using AsyncTest.Database.Database.Model;
 
 namespace AsyncTest.Database.Database
 {
+    [SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly")]
     public class DatabaseContext : DbContext, IDatabaseContext
     {
         public DbSet<BalloonEntity> Balloons { get; set; }
